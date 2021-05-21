6a5ac8c7570294c3aaabc29ee4b1d52434351b11
franceska.padillacoo <franceska.padillacoo@bishopireton.org>
2021-05-21 17:13:22 -0400

    Now extracts track features

--- spotifyPlaylistModifier/source.py
+++ spotifyPlaylistModifier/source.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = 
-client_secret = 
+client_id = ''
+client_secret = ''
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
                                                       client_secret=client_secret)
@@ -18,11 +18,33 @@ track_ids = []
 
 for track in results['tracks'][:10]:
     track_names.append(track['name'])
+    print('adding ' + track['name'])
     track_uris.append(track['uri'])
+    print('uri: ' + track['uri'])
     track_ids.append(track['id'])
+    print('id: ' + track['id'])
 
+spotifyTrackFeatures = []
 for track in track_ids:
-    print(spotify.audio_analysis(track))
+    spotifyTrackFeatures.append(spotify.audio_features(track))
+
+songsWFeatures = {}
+for i in range(10):
+    trackFeatures = {}
+    trackFeatures['danceability'] = spotifyTrackFeatures[i][0]['danceability']
+    trackFeatures['energy'] = spotifyTrackFeatures[i][0]['energy']
+    trackFeatures['loudness'] = spotifyTrackFeatures[i][0]['loudness']
+    trackFeatures['acousticness'] = spotifyTrackFeatures[i][0]['acousticness']
+    trackFeatures['instrumentalness'] = spotifyTrackFeatures[i][0]['instrumentalness']
+    trackFeatures['speechiness'] = spotifyTrackFeatures[i][0]['speechiness']
+    trackFeatures['valence'] = spotifyTrackFeatures[i][0]['valence']
+    songsWFeatures[track_names[i]] = trackFeatures
+
+print(songsWFeatures)
+    
+    
+    
+    
 
 
 
